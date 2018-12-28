86c7e0131ca7a9cc46d308581bfb90d83a8e5d54
ryuuta0217 <ryuuta.iwakura@gmail.com>
2018-12-28 09:35:17 +0900

    ちょこちょこ修正
    matches使う必要のないところとか配列にしなくていいやつとか

--- src/main/java/com/ryuuta0217/sampleJDA/listeners/CommandListener.java
+++ src/main/java/com/ryuuta0217/sampleJDA/listeners/CommandListener.java
@@ -12,20 +12,20 @@ public class CommandListener extends ListenerAdapter {
         if (event.getAuthor().equals(event.getJDA().getSelfUser())) return;
 
         //pingコマンド
-        if (event.getMessage().getContentRaw().matches("^(?i)!ping$")) {
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!ping")) {
             event.getChannel().sendMessage("Pong!").queue();
             event.getChannel().sendMessage("WebSocket Ping is " + event.getJDA().getPing() + "ms").queue();
             return;
         }
 
         //pinのみ
-        if (event.getMessage().getContentRaw().matches("^(?i)!pin$")) event.getChannel().sendMessage("`!pin <enable/disable>`").queue();
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!pin")) event.getChannel().sendMessage("`!pin <enable/disable>`").queue();
 
         //pin+オプション付
         if (event.getMessage().getContentRaw().matches("^(?i)!pin .*")) {
-            String[] args = event.getMessage().getContentRaw().replaceAll("^!pin ", "").split(" ");
-            if (args[0].equalsIgnoreCase("enable")) PINDOME_ENABLED = true;
-            else if (args[0].equalsIgnoreCase("disable")) PINDOME_ENABLED = false;
+            String args = event.getMessage().getContentRaw().replaceFirst("^!pin ", "");
+            if (args.equalsIgnoreCase("enable")) PINDOME_ENABLED = true;
+            else if (args.equalsIgnoreCase("disable")) PINDOME_ENABLED = false;
             else return;
             event.getChannel().sendMessage(PINDOME_ENABLED ? "ピン留めくん機能を有効化しました。" : "ピン留めくん機能を無効化しました。").queue();
         }
